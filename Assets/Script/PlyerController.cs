434a7731b642e71304b73c3fb1ad46341b076726
rimayu0815 <rikussyuu@gmail.com>
2021-02-01 23:41:15 +0900

    1.ステージ作成 2.プレイヤー作成 3.スクリプトで上下左右移動（ただし滑るので修正必要）

---
+++ Assets/Script/PlyerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    private float x;
+    private float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+
+    }
+
+    public void PlayerMove()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        if (x > 0 || x < 0 || y > 0 || y < 0)
+        {
+           rb.velocity = new Vector2(x * speed, y * speed);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
